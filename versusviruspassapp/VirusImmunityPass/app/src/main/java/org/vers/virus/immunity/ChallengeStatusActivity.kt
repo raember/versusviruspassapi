cc5e298d47e20a87496ad40524d15c0a0d764d16
lbenno <benno.luthiger@id.ethz.ch>
2020-04-05 12:56:21 +0200

    Added first version of android app.

---
+++ versusviruspassapp/VirusImmunityPass/app/src/main/java/org/vers/virus/immunity/ChallengeStatusActivity.kt
@@ -0,0 +1,12 @@
+package org.vers.virus.immunity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChallengeStatusActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_challenge_status)
+    }
+}
